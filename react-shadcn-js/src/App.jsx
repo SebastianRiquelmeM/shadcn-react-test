4eb9b839f7a199879311db9a8b596105dcf26f4a
SebAstián Riquelme <riquelmemenuzsebastian@gmail.com>
2024-04-07 17:25:11 -0400

    First component button

---
+++ react-shadcn-js/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+	return (
+		<>
+			<h1>Hello from Shadcn UI!</h1>
+			<Button>Click me</Button>
+		</>
+	);
+}
+
+export default App;
